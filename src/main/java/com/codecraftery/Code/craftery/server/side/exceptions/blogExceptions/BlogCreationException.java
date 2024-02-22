f17553928e226a571e19e6f829e8fe316fb23449
Natasa <natasa.t.markovic@gmail.com>
2024-02-22 23:43:09 +0100

    BlogCreationException

--- src/main/java/com/codecraftery/Code/craftery/server/side/exceptions/blogExceptions/BlogCreationException.java
+++ src/main/java/com/codecraftery/Code/craftery/server/side/exceptions/blogExceptions/BlogCreationException.java
@@ -1,6 +1,6 @@
 package com.codecraftery.Code.craftery.server.side.exceptions.blogExceptions;
 
-public class BlogCreationException extends Exception {
+public class C extends Exception {
     public BlogCreationException(String message, Throwable cause) {
         super(message, cause);
     }
